62179ff189ad6f424d88acd9b29a6d52795d8c4a
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2023-05-22 21:35:51 -0400

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,10 @@ function iniciar(){
     formulario.style.display = 'flex';
 }
 
-function confere1(){
+function confere1(event){
     //debugger;
     let botoes = document.getElementsByName('q1');
-
+    
     for(let i = 0; i < botoes.length; i++){
         if(botoes[i].checked){
             console.log(`opção ${i+1} selecionada`);
