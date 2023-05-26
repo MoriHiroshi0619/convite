091136845f0a0a79339f043cacdee0163cbf7535
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2023-05-26 00:03:58 -0400

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,26 @@ function iniciar(){
     formulario.style.display = 'flex';
 }
 
-function confere1(){
+function confereResposta(res){
     //debugger;
+    let questao1 = document.querySelector('.ques1');
     let botoes = document.getElementsByName('q1');
+    let questao2 = document.querySelector('.ques2');
     
     for(let i = 0; i < botoes.length; i++){
         if(botoes[i].checked){
             console.log(`opção ${i+1} selecionada`);
+            console.log(botoes[i].value);
+            if(botoes[i].value == res){
+                questao2.classList.add('aparece');
+                questao1.classList.add('acerto');
+            }
+            if(botoes[i].value != res){
+                questao1.classList.add('error');
+                setTimeout(()=>{
+                    questao1.classList.remove('error');
+                },500);
+            }
         }
     }
 }
